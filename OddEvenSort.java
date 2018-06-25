68f323b48c3ca70e33883dec1feba22545d9ff2a
joshuacassidy <Joshcassidy79@gmail.com>
2018-06-25 13:38:36 +0100

    Modularised code

--- OddEvenSort.java
+++ OddEvenSort.java
@@ -3,24 +3,23 @@ public class OddEvenSort {
     public int[] sort(int[] arr) {
         boolean sorted = false;
         while(!sorted) {
-            sorted = true;
-            for(int i = 1; i < arr.length-1; i += 2) {
-                if(arr[i] > arr[i+1]) {
-                    swap(arr, i, i+1);
-                    sorted = false;
-                }
-            }
-
-            for(int i = 0; i < arr.length-1; i += 2) {
-                if(arr[i] > arr[i+1]) {
-                    swap(arr, i, i+1);
-                    sorted = false;
-                }
-            }
+            sortHelper(arr, 0);
+            sorted = sortHelper(arr, 1);
         }
 
         return arr;
     }
+
+    public boolean sortHelper(int[] arr, int startIndex) {
+        for(int i = startIndex; i < arr.length-1; i += 2) {
+            if(arr[i] > arr[i+1]) {
+                swap(arr, i, i+1);
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     public void swap(int[] list, int i, int j) {
         int temp = list[i];
